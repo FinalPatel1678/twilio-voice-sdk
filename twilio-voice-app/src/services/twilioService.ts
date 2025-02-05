94fa4753658e228f00e6e4fd30573327ccb21fb4
final-patel1603 <fpatel@in.allmysons.com>
2025-02-05 14:17:38 +0530

    chore: add envs samples

--- twilio-voice-app/src/services/twilioService.ts
+++ twilio-voice-app/src/services/twilioService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Fetch the Twilio access token
 export const getAccessToken = async (): Promise<{ token: string }> => {
   try {
-    const response = await axios.get<{ token: string }>('http://localhost:3001/access_token');
+    const response = await axios.get<{ token: string }>(`${process.env.REACT_APP_BASE_URL}/access_token`);
     return response.data;
   } catch (error) {
     console.error('Error fetching access token:', error);
